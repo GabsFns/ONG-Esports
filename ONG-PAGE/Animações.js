36e5d47ff2ee11f35650da5a78f8bfdb89874b64
GabsFns <gabriel0917py@gmail.com>
2023-10-23 21:15:56 -0300

    Pagina de login completa e tela de vagas feita

--- ONG-PAGE/Animações.js
+++ ONG-PAGE/Animações.js
@@ -9,3 +9,12 @@ box.addEventListener('mouseout', () => {
     box.style.transform = 'translateX(0)';
     box.style.backgroundColor = '#3498db';
 });
+function showTooltip(element) {
+    var tooltip = element.querySelector('.tooltiptext');
+    tooltip.style.display = 'block';
+}
+
+function hideTooltip(element) {
+    var tooltip = element.querySelector('.tooltiptext');
+    tooltip.style.display = 'none';
+}
